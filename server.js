f8313bd312c8e3a7baaf751a8bb1746f537b5206
UrielAndrade <andradeleiru@gmail.com>
2025-01-31 23:23:57 -0400

    segundo commint: adicionei a rota NewTask, que adiciona novos users e ja joga ele pro mangoDB

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 //? methodo 2 mais EZ mais antigo, (nao compensa, abandonado aos poucos!)
 //!const express = require('express');
-
 //? Metodo 1 de "Puxar" a biblioteca express, (Mais chato minh opn, entretanto, mais moderno), ADD no JSON "packge.json" = "type": "module", 
+
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 const app = express();
-const port = 3000;
+app.use(express.json()); //? Habilita o Express a ler JSON!
 
-app.get("/tasks", (req, res) => {
-        res.send('ok, Entrou na rota usuarios!');
+//? cadastrar task
+app.post('/NewUser', async(req, res) => { //? Tudo vem dentro das 
+    try {
+        const newUser = await prisma.user.create({ //!Retorna uma promise 'async' (assincrona)
+            data: {
+                desc: req.body.desc,
+                name: req.body.name,
+                age: req.body.age,
+            },
+        });
+        res.status(201).json(newUser); // Retorne o usuario criado
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao criar usuário' }); //? erros
+    }
+});
 
+//? exibir dados 
+app.get("/users", (req, res) => {
+        res.status(200).json(users);
     }) //1) tipo de rota / metodo HTTP 2) endereco
-app.listen(3000)
+app.listen(3000);
+//
